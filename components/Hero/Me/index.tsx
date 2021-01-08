c3ae2e08ab9db7b3846856228a24ed5b73c84e3e
Maximilian Torggler <maximilian.torggler@tuta.io>
2021-01-08 19:18:54 +0100

    feat: full body img animation

--- components/Hero/Me/index.tsx
+++ components/Hero/Me/index.tsx
@@ -1,14 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import styles from "./index.module.css";
 
 interface Props {}
 
+const numImages = 8;
+
 const Me = ({}: Props) => {
+  const [currImg, setImg] = useState<number>(0);
+
+  useEffect(() => {
+    let didCancel = false;
+    const interval = window.setInterval(() => {
+      let nextIdx = currImg + 1;
+      if (nextIdx >= numImages) {
+        nextIdx = 0;
+      }
+      if (!didCancel) {
+        setImg(nextIdx);
+      }
+    }, 1000);
+
+    return () => {
+      window.clearInterval(interval);
+      didCancel = true;
+    };
+  }, [currImg]);
+
   return (
     <div className={styles["me"]}>
       <div className={styles["me-img-container"]}>
-        <Image src="/illustrations/me1.png" layout="fill" objectFit="contain" />
+        <Image src={`/me/${currImg}.png`} layout="fill" objectFit="contain" />
       </div>
     </div>
   );
